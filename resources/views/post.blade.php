caea01deb918a9e06f0317049d080406a463b203
darkopet <darkodepetrovski@gmail.com>
2022-06-08 13:26:40 +0200

    Eloquent Query Filter.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -28,7 +28,7 @@
                                     </a>    
                             </div>
                                     <div class="space-x-2">
-                                    <a href="/categories/{{ $post->category->slug }}"
+                                    <a href="/main_site/?category={{ $post->category->slug }}"
                                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
                                        style="font-size: 10px">
                                           {{ $post->category->name }}
