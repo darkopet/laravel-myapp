2d991ce319dd23ef0af420a47d4557dff0fc1688
darkopet <darkodepetrovski@gmail.com>
2022-06-08 14:34:49 +0200

    BadMethodCallException

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,20 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post2;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('main_site', [
+        return view('main_site.index', [
             'main_site' => Post2::latest()->filter(request(['search','category']))->get()
         ]);
     }
 
-    
+
     public function show(Post2 $post)
     {
-        return view('post', [
+        return view('post.show', [
             'post' => $post
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Post2;
 use App\Models\User;
 use App\Models\Category;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -22,44 +23,23 @@ Route::get('/', function(){
     return view('welcome'); 
 });
 
-
-Route::get('main_site', function()
-{                 
-    $posts = Post2::latest();
-    if(request('search'))
-    {
-        $posts
-            ->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') .'%');
-    }
-
-    return view('main_site', [ 
-        'main_site'=> $posts->get(), 
-        'categories' => Category::all() 
-    ]);      
-})->name('home');
+Route::get('main_site', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 
-Route::get('posts/{post:slug}', function(Post2 $post)
-  {
-    return view('post', ['post'=> $post]);  
-  });
-
-
-Route::get('categories/{category:slug}', function(Category $category)
-{
-    return view('main_site', [
-        'main_site' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()     
-    ]);
-})->name('category');
+// Route::get('categories/{category:slug}', function(Category $category)
+// {
+//     return view('main_site', [
+//         'main_site' => $category->posts,
+//         'currentCategory' => $category,
+//         'categories' => Category::all()     
+//     ]);
+// })->name('category');
 
 
 Route::get('authors/{author:username}', function(User $author){
     //dd($author);
     return view('main_site', [ 
-        'main_site' => $author->posts,
-        'categories' => Category::all() 
+        'main_site' => $author->posts
     ]); 
 });
