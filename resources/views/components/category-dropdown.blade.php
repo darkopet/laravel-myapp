af27e08a5b041c96f5ecf7fba9221c7f37aaa4d9
darkopet <darkodepetrovski@gmail.com>
2022-06-09 17:27:18 +0200

    Category Search Merging.

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -11,14 +11,16 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item href="/posts" :active="request()->routeIs('home')">
+    <x-dropdown-item href="/?{{ http_build_query(request()->except('category', 'page')) }}" 
+                     :active="request()->routeIs('home')">
         All
     </x-dropdown-item>
     
     @foreach ($categories as $category)
-        <x-dropdown-item href="/posts?category={{ $category->slug }}" 
-                         :active='request()->is("categories/{$category->slug}")'>
-            {{ ucwords($category->name) }}
+        <x-dropdown-item 
+            href="/posts?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}" 
+            :active='request()->is("categories/{$category->slug}")'>
+                {{ ucwords($category->name) }}
         </x-dropdown-item>
     @endforeach
 </x-dropdown>
