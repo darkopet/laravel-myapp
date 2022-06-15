091a88b5bf43e62743a89bda946731f2dd594595
darkopet <darkodepetrovski@gmail.com>
2022-06-15 16:15:28 +0200

    Images Back

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -4,7 +4,6 @@
             <div class="flex-shrink-0">
                 <img src="https://i.pravatar.cc/60?u={{ $comment->user_id }}" alt="" width="60" height="60" class="rounded-xl">
             </div>
-
             <div>
                 <header class="mb-4">
                     <h3 class="font-bold">{{ $comment->author->username }}</h3>
