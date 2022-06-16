637a0709ad5be9e163e65669134be039e2aae316
darkopet <darkodepetrovski@gmail.com>
2022-06-16 12:32:34 +0200

    Edit OK

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -48,7 +48,7 @@ class AdminPostController extends Controller
         $attributes = request()->validate([
             'title' => 'required',
             'thumbnail' => 'image',
-            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post->id)],
+            'slug' => ['required', Rule::unique('post2s', 'slug')->ignore($post->id)],
             'excerpt' => 'required',
             'body' => 'required',
             'category_id' => ['required', Rule::exists('categories', 'id')]
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -19,7 +19,7 @@
                                     </td>
 
                                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <a href="/posts/admin/{{ $post->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
+                                        <a href="/admin/{{ $post->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
                                     </td>
 
                                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
