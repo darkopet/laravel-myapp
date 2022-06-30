d1bc65f79dcf6772d8e2a66b80840f365b7562b9
darkopet <darkodepetrovski@gmail.com>
2022-06-30 12:45:33 +0200

    format form.comps

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,6 +1,7 @@
 @props(['name'])
 
 <x-form.field>
+
     <x-form.label name="{{ $name }}"/>
 
     <input class="border border-gray-400 p-2 w-full"
@@ -12,4 +13,5 @@
     >
 
     <x-form.error name="{{ $name }}"/>
+    
 </x-form.field>
